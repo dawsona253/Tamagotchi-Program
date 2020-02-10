adcbcbdbab1d4e5ac2c02def72886b0a358150a5
Brevin Cronk and Sasa Schwartz <brevin@>
2020-02-10 09:04:37 -0800

    initial file structure

--- src/main.js
+++ src/main.js
@@ -2,7 +2,5 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 // import $ from 'jquery';
-import { thingFunction } from './thing.js'
+// import { Tamagotchi } from './tamagotchi';
 
-
-console.log(thingFunction);
